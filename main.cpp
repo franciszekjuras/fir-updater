73dcb8016d37a679b40eb07d5a10d83e9b6b93b8
Franciszek Juras <franciszekjuras@gmail.com>
2019-10-05 19:19:03 +0200

    Moved app specific strings to pro file. Added recovery in case of update failure.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <QProgressDialog>
 #include <QMessageBox>
 #include <QIcon>
+#include <QProcess>
 
 #include <fstream>
 
@@ -36,14 +37,19 @@ void logToFile(QtMsgType type, const QMessageLogContext &context, const QString 
   logfile.flush();
 }
 
+void failureMessage(){
+    QMessageBox::critical(nullptr, APPNAME, "Could not apply update.");
+}
+
 int main(int argc, char *argv[])
 {
 
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon(":data/icon.png"));
 
-    QSystemSemaphore sem( "firgui_update", 1, QSystemSemaphore::Open );
+    QSystemSemaphore sem( APPSEMAPHORE, 1, QSystemSemaphore::Open );
     sem.acquire();
+
 
     //Just for testing
 //    QDir wdir;
@@ -57,7 +63,7 @@ int main(int argc, char *argv[])
     qInfo() << "Start.";
 
     QProgressDialog progress("Applying update.", "", 0, 100);
-    progress.setWindowTitle("FIR Controller");
+    progress.setWindowTitle(APPNAME);
     progress.setWindowModality(Qt::WindowModal);
     progress.setMinimumDuration(0);
     progress.setWindowFlags(Qt::Dialog | Qt::WindowSystemMenuHint | Qt::WindowTitleHint);
@@ -65,28 +71,46 @@ int main(int argc, char *argv[])
 
     progress.setValue(0);
 
-    QFile updateStatusFile("updatestatus.txt");
+    QFile updateStatusFile("updatestatus");
     if(!updateStatusFile.open(QFile::WriteOnly)){
         qCritical()<< "Could not open file for writing update status.";
-        QMessageBox::critical(nullptr, "Fir Controller", "Could not apply update."); return -1;
+        failureMessage(); return -1;
     }
     QTextStream updateStatus(&updateStatusFile);
     updateStatus << "fail";
 
-    progress.setValue(5);
+#ifdef _WIN32
+    QFile exeFile(QString(APPEXE)+".exe");
+#else
+    QFile exeFile(APPEXE);
+#endif
+    if(!exeFile.open(QIODevice::ReadWrite | QIODevice::ExistingOnly)){
+        qCritical() << "Could not open executable file with read and write rights.";
+        QMessageBox::critical(nullptr, "FIR Controller", "Could not apply update because application files are in use.");
+        return -1;
+    }
+    exeFile.close();
 
     QDir cdir;
     if(!cdir.exists("update")){
         qCritical() << "Update directory does not exist.";
-        QMessageBox::critical(nullptr, "Fir Controller", "Could not apply update."); return -1;
+        failureMessage(); return -1;
     }
     if(!cdir.exists("update/updater/updater.exe")){
         qCritical() << "Updater executable is not in place.";
-        QMessageBox::critical(nullptr, "Fir Controller", "Could not apply update."); return -1;
+        failureMessage(); return -1;
     }
     if(!cdir.exists("update/app")){
         qCritical() << "Application directory does not exist";
-        QMessageBox::critical(nullptr, "Fir Controller", "Could not apply update."); return -1;
+        failureMessage(); return -1;
+    }
+#ifdef _WIN32
+    if(!cdir.exists(QString("update/app/") + APPEXE +".exe")){
+#else
+    if(!cdir.exists(QString("update/app/") + APPEXE)){
+#endif
+        qCritical() << "Application executable does not exist";
+        failureMessage(); return -1;
     }
 
     progress.setValue(10);
@@ -96,8 +120,8 @@ int main(int argc, char *argv[])
         qInfo() << "Deleting old backup.";
         QDir bkdir(cdir); bkdir.cd("backup");
         if(!bkdir.removeRecursively()){
-            qInfo() << "Could not remove old backup.";
-            QMessageBox::critical(nullptr, "Fir Controller", "Could not apply update."); return -1;
+            qCritical() << "Could not remove old backup.";
+            failureMessage(); return -1;
         }
     }
 
@@ -105,25 +129,67 @@ int main(int argc, char *argv[])
     mainDirContent.removeOne("data");
     mainDirContent.removeOne("update");
     mainDirContent.removeOne("updatelog.txt");
-    mainDirContent.removeOne("updatestatus.txt");
+    mainDirContent.removeOne("updatestatus");
     cdir.mkdir("backup");
     {
         int i = 0; int s = mainDirContent.length();
         for(auto it : mainDirContent){
-            qInfo() << "Moved" << it <<QFile::rename(it, "backup/" + it);
+            bool success = QFile::rename(it, "backup/" + it);
+            qInfo() << "Moved" << it << success;
+            if(!success){
+                 for(auto it2 : mainDirContent){
+                     if(it2 == it) break;
+                     success = QFile::rename("backup/" + it, it);
+                     qInfo() << "Moved back" << "backup/" + it << success;
+                     if(!success){
+                         qCritical() << "Could not apply backup. Application files corrupted.";
+                         QMessageBox::critical(nullptr, APPNAME, QString("Update corrupted application files. Download app manually from ") + APPSOURCE);
+                         return -1;
+                     }
+                 }
+                 //update failed but changes reverted
+                 qCritical() << "Could not remove application files. Original state recovered from backup.";
+                 QMessageBox::critical(nullptr, APPNAME, "Could not apply update because application files are in use or updater has no rights to move them.");
+                 return -1;
+            }
             progress.setValue(10+(40*(++i/s)));
         }
     }
     QDir appdir(cdir);
     if(!appdir.cd("update/app")){
         qCritical() << "Could not cd to update/app.";
-        QMessageBox::critical(nullptr, "Fir Controller", "Could not apply update."); return -1;
+        failureMessage(); return -1;
     }
     auto appDirContent = appdir.entryList(QDir::NoDotAndDotDot | QDir::Dirs | QDir::Files);
     {
-        int i = 0; int s = mainDirContent.length();
+        int i = 0; int s = appDirContent.length();
         for(auto it : appDirContent){
-            qInfo() << "Moved" << it <<QFile::rename("update/app/" + it,  it);
+            bool success = QFile::rename("update/app/" + it,  it);
+            qInfo() << "Moved" << it << success;
+            if(!success){
+                for(auto it2 : appDirContent){
+                    if(it2 == it) break;
+                    success = QFile::rename(it, "update/app/" + it);
+                    qInfo() << "Moved back" << it << success;
+                    if(!success){
+                        qCritical() << "Could not revert update. Application files corrupted.";
+                        QMessageBox::critical(nullptr, APPNAME, "Update corrupted application files. Download app manually from https://github.com/franciszekjuras/firgui/releases/latest");
+                        return -1;
+                    }
+                }
+                for(auto it3 : mainDirContent){
+                    success = QFile::rename("backup/" + it, it);
+                    qInfo() << "Moved back" << "backup/" +  it << success;
+                    if(!success){
+                        qCritical() << "Could not apply backup. Application files corrupted.";
+                        QMessageBox::critical(nullptr, APPNAME, "Update corrupted application files. Download app manually from https://github.com/franciszekjuras/firgui/releases/latest");
+                        return -1;
+                    }
+                }
+                qCritical() << "Could not move update files. Original state recovered from backup.";
+                QMessageBox::critical(nullptr, APPNAME, "Could not apply update because update files are in use or updater has no rights to move them.");
+                return -1;
+            }
             progress.setValue(50+(49*(++i/s)));
         }
     }
@@ -138,8 +204,21 @@ int main(int argc, char *argv[])
     updateStatus.seek(0);
     updateStatusFile.resize(0);
     updateStatus << "ok";
+    updateStatus.flush();updateStatusFile.close();
     QThread::sleep(1);
     progress.setValue(100);
+#ifdef _WIN32
+    if(cdir.exists(QString(APPEXE)+".exe")){
+        if(!QProcess::startDetached(QString("\"") + APPEXE +".exe\"")){
+#else
+    if(cdir.exists(APPEXE)){
+        if(!QProcess::startDetached(QString("\"") + APPEXE +"\"")){
+#endif
+            qWarning() << "Could not restart application";
+        }
+    } else{
+        qWarning() << "Could not find application executable";
+    }
     qInfo() << "End.";
     return 0;
 }
--- updater.pro
+++ updater.pro
@@ -4,6 +4,12 @@ CONFIG += c++11 core
 CONFIG -= app_bundle
 
 DEFINES += QT_DEPRECATED_WARNINGS
+__APPNAME = "FIR Controller"
+__APPEXE = "FIR Controller"
+DEFINES += APPSEMAPHORE=\"\\\"firgui_update\\\"\"
+DEFINES += APPSOURCE=\"\\\"https://github.com/franciszekjuras/firgui\\\"\"
+DEFINES += APPNAME=\"\\\"$${__APPNAME}\\\"\"
+DEFINES += APPEXE=\"\\\"$${__APPEXE}\\\"\"
 
 SOURCES += \
         main.cpp
